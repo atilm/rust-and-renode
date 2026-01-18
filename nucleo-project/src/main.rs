e515916b80cfd01d24e39e52c6c92ca8cb1247c7
Thielmann, Andreas <Andreas.Thielmann@berthold.com>
2026-01-18 15:53:53 +0100

    add usart2 to embbedded application

--- nucleo-project/src/main.rs
+++ nucleo-project/src/main.rs
@@ -5,7 +5,7 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{delay::Delay, pac, prelude::*}; // STM32F1 specific functions
+use stm32f1xx_hal::{delay::Delay, pac::{self}, prelude::*, serial::{Config, Serial}}; // STM32F1 specific functions
 #[allow(unused_imports)]
 use panic_halt; // When a panic occurs, stop the microcontroller
 
@@ -15,6 +15,7 @@ fn main() -> ! {
     let cp =  cortex_m::Peripherals::take().unwrap();
 
     let rcc = dp.RCC.constrain();
+    let mut afio = dp.AFIO.constrain();
 
     let mut gpio_a = dp.GPIOA.split();
     let mut gpio_c = dp.GPIOC.split();
@@ -26,8 +27,21 @@ fn main() -> ! {
     let clocks = rcc.cfgr.sysclk(8.mhz()).freeze(&mut flash.acr);
     let mut delay = Delay::new(cp.SYST, clocks);
 
+    // USART2 TX on PA2, RX on PA3
+    let tx = gpio_a.pa2.into_alternate_push_pull(&mut gpio_a.crl);
+    let rx = gpio_a.pa3;
+    let serial = Serial::usart2(
+        dp.USART2,
+        (tx, rx),
+        &mut afio.mapr,
+        Config::default().baudrate(9600.bps()),
+        clocks,
+    );
+    let (mut tx, _rx) = serial.split();
+
     let mut led_state = false;
     let mut prev_button_state = button.is_high();
+    let led_toogled = b"LED TOGGLED\r\n";
     loop {
         let curr_button_state = button.is_high();
         // Detect rising edge: prev low, curr high
@@ -35,8 +49,14 @@ fn main() -> ! {
             led_state = !led_state;
             if led_state {
                 led.set_high();
+                for byte in led_toogled.iter() {
+                    let _ = tx.write(*byte);
+                }
             } else {
                 led.set_low();
+                for byte in led_toogled.iter() {
+                    let _ = tx.write(*byte);
+                }
             }
         }
         prev_button_state = curr_button_state;
